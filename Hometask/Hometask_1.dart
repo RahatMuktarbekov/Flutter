bcdde6c43afcbdec4f88eded32b578ae6bd6d730
RahatMuktarbekov <119924435+RahatMuktarbekov@users.noreply.github.com>
2022-12-07 15:27:33 +0600

    DZ_2

---
+++ Hometask/Hometask_1.dart
@@ -0,0 +1,12 @@
+void main() {
+  int chocolate = 55;
+  int coffee = 66;
+  int milk = 77;
+  print(chocolate + coffee + milk);
+
+  int a = 2;
+  int b = a * 3;
+  int c = a + b;
+  print(c);
+
+ 
